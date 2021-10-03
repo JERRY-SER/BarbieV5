265f26e338f89fb03f9aa86b4157395abcf062d2
JERRY-SER <91775413+JERRY-SER@users.noreply.github.com>
2021-10-03 13:04:43 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "cutie.gif";
+  r_text[0] = "doll.png";
     
     
     var i = Math.floor(1*Math.random())
