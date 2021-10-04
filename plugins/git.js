ffbadda9a15d3bd1f4f6dd730b9203b3b5db5599
JERRY-SER <91775413+JERRY-SER@users.noreply.github.com>
2021-10-04 16:05:47 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -19,11 +19,11 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-*Sticker commads : https://github.com/JERRY-SER/BarbieV5/tree/master/uploads*
+*Creator number : wa.me/919567091981?text=Hi%20Tbc%20Jerry.%20*
+*Subscribe my channel : https://youtube.com/channel/UClhrCK46MWPHZdZUGkitKug*
+*Githublink (Setup)  :    https://github.com/JERRY-SER/BarbieV5*
+*Audio commands :   https://github.com/JERRY-SER/BarbieV5/tree/master/uploads*
+*Sticker commands : https://github.com/JERRY-SER/BarbieV5/tree/master/uploads*
 `}) 
 
 }));
